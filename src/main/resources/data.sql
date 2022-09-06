8dc5d038062b1e3eb4b14043696e6bee49afe3cd
Jordi Gómez Hidalgo <31970428+gnugomez@users.noreply.github.com>
2022-09-06 17:01:15 +0200

    chore(security): implement bcrypt and different user roles (#4)
    * feat(security): first implementation
    
    * chore(security): implement bcrypt and different user roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (username, password, user_role, first_name, last_name )
-VALUES ('user', '$2a$10$RWtvCLEhScW2tKFvuRz16eZJlRd6BCev0JiXEo6WpWMusVvvK2TYG', 'USER', 'Bob', 'Doe');
+VALUES ('user', '$2a$10$urd8xAkLbWc.wNWvzGl0L.t4dy05uJDytS5I0RUjmfA/vPp7DFakK', 'USER', 'Bob', 'Doe');
 
 INSERT INTO users (username, password, user_role, first_name, last_name)
-VALUES ('admin', '$2a$10$RWtvCLEhScW2tKFvuRz16eZJlRd6BCev0JiXEo6WpWMusVvvK2TYG', 'ADMIN', 'Alice', 'Doe');
+VALUES ('admin', '$2a$10$urd8xAkLbWc.wNWvzGl0L.t4dy05uJDytS5I0RUjmfA/vPp7DFakK', 'ADMIN', 'Alice', 'Doe');
+
